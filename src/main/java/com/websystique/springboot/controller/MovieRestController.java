eac0fa74ab65d60ddccd8c0e15b0a9d123e3ddda
marichkaK <maria199881@gmail.com>
2019-05-08 13:02:21 +0300

    add statistic end point

--- src/main/java/com/websystique/springboot/controller/MovieRestController.java
+++ src/main/java/com/websystique/springboot/controller/MovieRestController.java
@@ -3,23 +3,15 @@ package com.websystique.springboot.controller;
 import com.websystique.springboot.dto.MovieDto;
 import com.websystique.springboot.model.Movie;
 import com.websystique.springboot.service.MovieService;
-import com.websystique.springboot.util.DateConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static com.websystique.springboot.util.DateConverter.convertToDateViaSqlTimestamp;
-import static com.websystique.springboot.util.DateConverter.formatfromString;
 
 @RestController
 @RequestMapping("/api/movies")
